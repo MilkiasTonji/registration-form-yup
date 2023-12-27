a60a854a5e50e7ba9651c1202a91df7658ac2d4f
Milkias Tonji <tongisttonji@gmail.com>
2023-12-27 16:19:29 +0300

    Added hamburger icon and made pages responsive to all devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,36 @@
-
+import { useState } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+ const handleToggle = () => {
+    const menu = document.getElementById("menu");
+    let value: any = menu?.classList.value || 'open';
+    setIsOpen(!isOpen);
+
+    if (isOpen) {
+      menu?.classList.remove(value);
+      menu?.classList.add("open");
+    } else {
+      menu?.classList.remove(value);
+      menu?.classList.add("close");
+    }
+ }
+
   return (
     <div className="container">
-        <ul>
-            <li>
-                <a href={'/home'}>Home</a>
-                <a href="/services">Services</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact</a>
-            </li>
-        </ul>
+      <button id="hamburger-icon" onClick={handleToggle}> ☰ </button>
+      <ul id="menu">
+        <li>
+          <a href={"/home"}>Home</a>
+          <a href="/services">Services</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
